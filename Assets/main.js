33e7489556b06720d5cccaf4f7c0a8be333ce327
mike <mikeweissbluth@gmail.com>
2018-04-06 11:55:21 -0500

    finalize version of this project

--- Assets/main.js
+++ Assets/main.js
@@ -13,6 +13,7 @@
     storageBucket: "trainscheduler-22735.appspot.com",
     messagingSenderId: "566416614236"
   };
+
   firebase.initializeApp(config);
 
   var database = firebase.database();
@@ -33,34 +34,52 @@ $("#add-Train-btn").on("click", function(event) {
     // Grabs user input
      trainName = $("#train-name-input").val().trim();
      destination = $("#destination-input").val().trim();
-     trainTime = $("#trainTime-input").val().trim();
+     trainTime = moment($("#trainTime-input").val().trim(),"hh:mm").format("X");
      frequency = $("#freq-input").val().trim();
 
      console.log(trainName);
      console.log(destination);
      console.log(trainTime);
      console.log(frequency);
+     var chooChoo = {
+        trainName: trainName,
+        destination: destination,
+        trainTime: trainTime,
+        frequency: frequency
+       };
+
+     database.ref().push(chooChoo);
 });
 
-//  // Creates local "temporary" object for holding train data
-//   var chooChoo = {
-//     trainName: trainName,
-//     destination: destination,
-//     trainTime: trainTime,
-//     frequency: frequency
-//   };
 
-// //  // Uploads trainSchedule data to the database
-// database.ref().push(chooChoo);
+ /** add data event */
+ database.ref().on('child_added', function(childSnapshot, prevChildKey){
+    console.log(childSnapshot.val());
+    console.log(childSnapshot.val().destination);
+    console.log(childSnapshot.val().trainName);
+    console.log(childSnapshot.val().trainTime);
+    console.log(childSnapshot.val().frequency);
 
-// //  // Logs everything to console
-//  // console.log(trainName);
-// //   console.log(destination);
-// //   console.log(trainTime);
-// //   console.log(frequency);
+    let tName=childSnapshot.val().trainName;
+    let tTime=childSnapshot.val().trainTime;
+    let tDestination=childSnapshot.val().destination;
+    let tFrequency=childSnapshot.val().frequency;
+   
+    
 
+//the tDifference equals some amount of minutes.
+  let tDifference=moment().subtract(moment.unix(tTime),'minutes');
+//now we want to see how many minutes are left over from some amount of frequency
+  let tRemainder=tDifference%tFrequency;
 
+  let tMinutes=tFrequency-tRemainder;
+  let tArrival=moment().add(tMinutes, 'm').format("hh:mm");
+  
 
+    $("#arrivals").append("<tr><td>"+tName+"</td><td>"+tDestination+"</td><td>"+tFrequency+"</td><td>"+tArrival+"</td><td>"+tMinutes+"</td></tr>")
+ })
+
+ // a new variable for each console log childsnapshot
 
 
  });
